48de403269ff3d156dba579448e9b5596c707ded
Anakaren <ak.rojas.07@gmail.com>
2024-01-15 10:50:15 -0800

    remove extras from wheel view

--- src/Components/wheel/wheelView.tsx
+++ src/Components/wheel/wheelView.tsx
@@ -3,10 +3,14 @@ import { IGlobalInputState } from '../../utils/globalStateProvider';
 
 // Wheel placeholder
 function WheelView ({inputs}: IGlobalInputState) {
-    return (    
-    <canvas>
-        {inputs}
-    </canvas>
+    return (
+        <>        
+            {inputs}
+            <canvas>
+                {inputs}
+            </canvas>
+        </>    
+
     )
 }
 
